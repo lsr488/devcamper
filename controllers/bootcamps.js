a5f35e9416e6c9eed8223ce79458909176d503af
unknown <lsr488@gmail.com>
2021-09-12 21:32:29 -0400

    Streamlined routes for all methods.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,47 @@
 // export methods so they can be brought in the server
 // these are essentially our middleware
+
+// @desc 			Get All bootcamps
+// @route 		GET /api/v1/bootcamps
+// @access 		Public
 exports.getBootcamps = (req, res, next) => {
-	res.status(200).json({ success: true, msg: 'Show all bootcamps.' });
+	res
+		.status(200)
+		.json({ success: true, msg: 'Show all bootcamps.' });
+}
+
+// @desc 			Get single bootcamp
+// @route 		GET /api/v1/bootcamps/:id
+// @access 		Public
+exports.getBootcamp = (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Show bootcamp # ${req.params.id}` });
+}
+
+// @desc 			Create single bootcamp
+// @route 		POST /api/v1/bootcamps
+// @access 		Private
+exports.createBootcamp = (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: 'Create new bootcamp.' });
+}
+
+// @desc 			Update single bootcamp
+// @route 		Put /api/v1/bootcamps/:id
+// @access 		Private
+exports.updateBootcamp = (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Update bootcamp # ${req.params.id}` });
+}
+
+// @desc 			Delete single bootcamp
+// @route 		Put /api/v1/bootcamps/:id
+// @access 		Private
+exports.deleteBootcamp = (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Delete bootcamp # ${req.params.id}` });
 }
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,33 +1,28 @@
 const express = require('express');
-const router = express.Router();
 
-// ### ROUTES ###
+// bring in controller methods
+const { 
+	getBootcamps,
+	getBootcamp,
+	createBootcamp,
+	updateBootcamp,
+	deleteBootcamp
+} = require('../controllers/bootcamps')
+
 // /api/v1/bootcamps
 // the full URL is pre-pended in the server
 
-// get all bootcamps from database
-router.get('/', (req, res) => {
+const router = express.Router();
 
-});
+// much more concisely creates the routes, with the logic all in the controller file
+router
+	.route('/')
+	.get(getBootcamps)
+	.post(createBootcamp);
 
-// get one bootcamp by id
-router.get('/:id', (req, res) => {
-	res.status(200).json({ success: true, msg: `Show bootcamp # ${req.params.id}` });
-});
-
-// create new bootcamp
-router.post('/', (req, res) => {
-	res.status(200).json({ success: true, msg: 'Create new bootcamp.' });
-});
-
-// update a bootcamp by id
-router.put('/:id', (req, res) => {
-	res.status(200).json({ success: true, msg: `Update bootcamp # ${req.params.id}` });
-});
-
-// delete a bootcamp by id
-router.delete('/:id', (req, res) => {
-	res.status(200).json({ success: true, msg: `Delete bootcamp # ${req.params.id}` });
-});
+router.route('/:id')
+	.get(getBootcamp),
+	.put(updateBootcamp),
+	.delete(deleteBootcamp)
 
 module.exports = router;
