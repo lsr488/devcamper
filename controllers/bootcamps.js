02eccf133c400d72f5108db7c9f7184aad109d8d
unknown <lsr488@gmail.com>
2021-09-28 20:15:13 -0400

    Fetch single record from db by id.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -21,10 +21,16 @@ exports.getBootcamps = async (req, res, next) => {
 // @desc 			Get single bootcamp
 // @route 		GET /api/v1/bootcamps/:id
 // @access 		Public
-exports.getBootcamp = (req, res, next) => {
-	res
-		.status(200)
-		.json({ success: true, msg: `Show bootcamp # ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+	try{
+		const bootcamp = await Bootcamp.findById(req.params.id);
+		res.status(200).json({
+			success: true,
+			data: bootcamp
+		});
+	} catch(err) {
+		res.status(400).json({ success: false });
+	}
 }
 
 // @desc 			Create single bootcamp
