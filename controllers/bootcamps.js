c47cd320a31cb05b996fac0e93df9f0d260b2d6d
unknown <lsr488@gmail.com>
2021-09-28 20:16:59 -0400

    Handle error message if ID not found.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -24,6 +24,11 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async (req, res, next) => {
 	try{
 		const bootcamp = await Bootcamp.findById(req.params.id);
+
+		if(!bootcamp) {
+			return res.status(400).json({ success: false, msg: 'ID not found.'});
+		}
+
 		res.status(200).json({
 			success: true,
 			data: bootcamp
