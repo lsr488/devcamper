8d72d549337f91c0f1622e0bc9968b6686e10ab3
unknown <lsr488@gmail.com>
2021-09-28 20:03:43 -0400

    Add single bootcamp to mongoDB.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -24,11 +24,22 @@ exports.getBootcamp = (req, res, next) => {
 // @desc 			Create single bootcamp
 // @route 		POST /api/v1/bootcamps
 // @access 		Private
-exports.createBootcamp = (req, res, next) => {
-	console.log(req.body);
-	res
-		.status(200)
-		.json({ success: true, msg: 'Create new bootcamp.' });
+exports.createBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.create(req.body);
+
+		res.status(201).json({
+			success: true,
+			data: bootcamp
+		});
+	} catch (err) {
+		res.status(400).json({ success: false });
+	}
+
+	// console.log(req.body);
+	// res
+	// 	.status(200)
+	// 	.json({ success: true, msg: 'Create new bootcamp.' });
 }
 
 // @desc 			Update single bootcamp
