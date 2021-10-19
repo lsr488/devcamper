f477294574ace77851f94bd19641330b9591fe8a
unknown <lsr488@gmail.com>
2021-10-19 09:35:15 -0400

    Custom error message handled by middleware.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -16,7 +16,7 @@ exports.getBootcamps = async (req, res, next) => {
 			data: bootcamps
 		})
 	} catch(err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 }
 
@@ -36,7 +36,7 @@ exports.getBootcamp = async (req, res, next) => {
 			data: bootcamp
 		});
 	} catch(err) {
-		next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+		next(err);
 	}
 }
 
@@ -52,7 +52,7 @@ exports.createBootcamp = async (req, res, next) => {
 			data: bootcamp
 		});
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(err);
 	}
 }
 
@@ -67,12 +67,12 @@ exports.updateBootcamp = async (req, res, next) => {
 		});
 
 		if(!bootcamp) {
-			return res.status(400).json({ success: false, msg: 'ID not found.'});
+			return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		}
 
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch(err) {
-			return res.status(400).json({ success: false, msg: 'ID not found.'});		
+			next(err);	
 	}
 }
 
@@ -84,11 +84,11 @@ exports.deleteBootcamp = async (req, res, next) => {
 		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
 		if(!bootcamp) {
-			return res.status(400).json({ success: false, msg: 'ID not found.'});
+			return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
 		}
 
 		res.status(200).json({ success: true, data: {} });
 	} catch(err) {
-			return res.status(400).json({ success: false, msg: 'ID not found.'});		
+			next(err);
 	}
 }
