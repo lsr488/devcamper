812581f7a3575322e74e86469fe14d9a57750eb9
unknown <lsr488@gmail.com>
2021-11-09 09:47:33 -0500

    Removed periods, cosmetic only

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -5,16 +5,16 @@ const geocoder = require('../utils/geocoder');
 const BootcampSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: [true, 'Please add a name.'],
+		required: [true, 'Please add a name'],
 		unique: true,
 		trim: true,
-		maxlength: [50, 'Name cannot be more than 50 characters.']
+		maxlength: [50, 'Name cannot be more than 50 characters']
 	},
 	slug: String,
 	description: {
 		type: String,
 		required: [true, 'Please add a name.'],
-		maxlength: [500, 'Name cannot be more than 500 characters.']
+		maxlength: [500, 'Name cannot be more than 500 characters']
 	},
 	website: {
     type: String,
@@ -25,7 +25,7 @@ const BootcampSchema = new mongoose.Schema({
   },
   phone: {
     type: String,
-    maxlength: [20, 'Phone number can not be longer than 20 characters.']
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
   },
   email: {
     type: String,
@@ -36,7 +36,7 @@ const BootcampSchema = new mongoose.Schema({
   },
   address: {
   	type: String,
-  	required: [true, 'Please add an address.']
+  	required: [true, 'Please add an address']
   },
   location: {
   	// GeoJSON Point
@@ -70,8 +70,8 @@ const BootcampSchema = new mongoose.Schema({
   },
   averageRating: {
   	type: Number,
-  	min: [1, 'Rating must be at least 1.'],
-  	max: [10, 'Rating cannot be more than 10.']
+  	min: [1, 'Rating must be at least 1'],
+  	max: [10, 'Rating cannot be more than 10']
   },
   averageCost: Number,
   photo: {
