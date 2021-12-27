9c6c518284a1aa8c9fbc099487e7fb5c5ffb3693
unknown <lsr488@gmail.com>
2021-12-27 14:24:11 -0500

    Add new single course.

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,15 @@
 const express = require('express');
 // bring in controller methods
-const { getCourses, getCourse } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
-router.route('/:id').get(getCourse);
+router
+	.route('/')
+	.get(getCourses)
+	.post(addCourse);
+router
+	.route('/:id')
+	.get(getCourse);
 
 module.exports = router;
