c53257b666644e0812ff2de60d33e57c1188fc09
unknown <lsr488@gmail.com>
2022-01-01 12:31:10 -0500

    Protect middleware created

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,35 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('./async');
 const ErrorResponse = require('../utils/errorResponse');
-const User = require('../models/User')
+const User = require('../models/User');
 
 // Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+	let token;
+
+	// check for authorization header
+	if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		token = req.headers.authorization.split(' ')[1];
+	} 
+	// else if(req.cookies) {
+	// 	token = req.cookies.token;
+	// }
+
+	// Make sure token exists
+	if(!token) {
+		return next(new ErrorResponse('Not authorized to access this route.'));
+	}
+
+	try {
+		// Verify token
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		
+		console.log(decoded); // DELETE ME
+
+		req.user = await User.findById(decoded.id);
+
+		next();
+	} catch(err) {
+		return next(new ErrorResponse('Not authorized to access this route.'));
+	}
+});
