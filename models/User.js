e1fcdf38ca1c33c323318de14d8c6d649d57cbfa
unknown <lsr488@gmail.com>
2022-01-01 11:06:21 -0500

    Correctly bring in UserSchema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const UserSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -34,4 +33,4 @@ const UserSchema = new mongoose.Schema({
 	}
 });
 
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('User', UserSchema);
