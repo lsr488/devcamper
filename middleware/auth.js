8ba1a782a91f5997fc69c87db644cf27a2ce34fa
unknown <lsr488@gmail.com>
2022-01-13 21:38:04 -0500

    Added status codes

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,20 +17,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
 	// Make sure token exists
 	if(!token) {
-		return next(new ErrorResponse('Not authorized to access this route.'));
+		return next(new ErrorResponse('Not authorized to access this route.', 401));
 	}
 
 	try {
 		// Verify token
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		
-		console.log(decoded); // DELETE ME
-
 		req.user = await User.findById(decoded.id);
 
 		next();
 	} catch(err) {
-		return next(new ErrorResponse('Not authorized to access this route.'));
+		return next(new ErrorResponse('Not authorized to access this route.', 401));
 	}
 });
 
