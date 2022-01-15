fbb9fcad20bde749553e30e659d88bb3675824ad
unknown <lsr488@gmail.com>
2022-01-15 12:10:23 -0500

    User routes created

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(cookieParser())
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 // dev logging middleware
 if(process.env.NODE_ENV === 'development') {
@@ -41,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', bootcamps); // this pre-pends the URL for bootcamps, so in the bootcamps routes, you don't need to include that in the URL
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 app.use(errorHandler);
 
