3c843b999265807bc542f3639941f626d3d90a7f
unknown <lsr488@gmail.com>
2022-01-15 12:08:51 -0500

    CRUD routes for users

--- controllers/users.js
+++ controllers/users.js
@@ -4,15 +4,58 @@ const User = require('../models/User');
 
 // @desc 			Get all users
 // @route 		GET /api/v1/users/
-// @access 		Public
-exports.register = asyncHandler(async (req, res, next) => {
-	// destructure to pull variables from req.body
-	const { name, email, password, role } = req.body;
+// @access 		Private/Admin
+exports.getUsers = asyncHandler(async (req, res, next) => {
+	res.status(200).json(res.advancedResults);
+});
 
-	// Create user
-	const user = await User.create({
-		name,
-		email,
-		password,
-		role
+// @desc 			Get single user
+// @route 		GET /api/v1/users/:id
+// @access 		Private/Admin
+exports.getUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.params.id);
+
+	res.status(200).json({
+		success: true,
+		data: user
 	});
+});
+
+// @desc 			Create user
+// @route 		POST /api/v1/users/
+// @access 		Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+	const user = await User.create(req.body);
+
+	res.status(201).json({
+		success: true,
+		data: user
+	});
+});
+
+// @desc 			Update user
+// @route 		PUT /api/v1/users/:id
+// @access 		Private/Admin
+exports.updateUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
+
+	res.status(200).json({
+		success: true,
+		data: user
+	});
+});
+
+// @desc 			Delete user
+// @route 		DELETE /api/v1/users/:id
+// @access 		Private/Admin
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+	await User.findByIdAndDelete(req.params.id);
+
+	res.status(200).json({
+		success: true,
+		data: {}
+	});
+});
