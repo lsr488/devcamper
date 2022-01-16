1e0e75f8de1a0f481578df53f3f7126518127366
unknown <lsr488@gmail.com>
2022-01-16 11:58:38 -0500

    Initial router

---
+++ routes/reviews.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { 
+	getReviews 
+} = require('../controllers/reviews');
+
+const Review = require('../models/Review');
+
+const router = express.Router({ mergeParams: true });
+
+// Middleware
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorize } = require('../middleware/auth');
+
+router
+	.route('/')
+		.get(advancedResults(Review, {
+			path:'bootcamp', // provides the whole object to be displayed
+			select: 'name description' // selects which properties to actually be displayed
+		}), 
+		getReviews
+);
+
+module.exports = router;
